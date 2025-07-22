51384c9176fc5a1d4b009111e3c490042a1ad871
OMAR ALI <omaralienig@gmail.com>
2025-07-22 12:44:17 +0100

    Refactor: Remove unused components and update app routes import statement

--- adbot_frontend/src/app/app.ts
+++ adbot_frontend/src/app/app.ts
@@ -1,38 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { User, UsersResponse } from './models/user.model';
-import { OrganizationalUnit, OUsResponse } from './models/ou.model';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, HttpClientModule],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
-  users: any[] = [];
-  ous: OrganizationalUnit[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.http.get<UsersResponse>('/api/users').subscribe({
-      next: (data) => {
-        this.users = data.users;
-      },
-      error: (error) => {
-        console.error('Error fetching users:', error);
-      }
-    });
-
-    this.http.get<OUsResponse>('/api/organizational-units').subscribe({
-      next: (data) => {
-        this.ous = data.organizational_units;
-      },
-      error: (error) => {
-        console.error('Error fetching OUs:', error);
-      }
-    });
-  }
+export class App {
+  // Component is now just a shell for routing
 }
